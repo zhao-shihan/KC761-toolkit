8121aaa952b2e1611fca055da5f0a5234c8283bc
zhaoshihan <zhao_shihan@foxmail.com>
2026-09-02 15:09:42 +0800

    chore(scripts): remove temporary calibration runner
    Delete the temp shell script used for invoking calib.py.

--- tmp.sh
+++
@@ -1,6 +0,0 @@
-python calib.py \
-    --label Am241 --data data/exp/2608a/am241-260825-data-subbkg.root --sim data/sim/am241-simulation-3e6.root --chlo 130 --chhi 165 \
-    --label K40 --data data/exp/2608b/k40-260831-data-subbkg.root --sim data/sim/k40-simulation-1e9.root --chlo 200 --chhi 950 \
-    --label Lu176 --data data/exp/2608b/lu176-260831-data-subbkg.root --sim data/sim/lu176-simulation-2e7.root --chlo 130 --chhi 450 \
-    --label Ra226 --data data/exp/2608b/ra226-260901-data-subbkg.root --sim data/sim/ra226-simulation-3e7.root --chlo 130 --chhi 1500 \
-    --label Th232 --data data/exp/2608b/th232-260831-data-subbkg.root --sim data/sim/th232-simulation-1e8.root --chlo 130 --chhi 1500
